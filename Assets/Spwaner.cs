d6ff2f3d3cf0907032b44ad69ae5ecf31d317209
bowsplinter <vivekkalyansk@gmail.com>
2017-03-12 15:52:30 +0800

    Add group of blocks and gamearea

---
+++ Assets/Spwaner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spwaner : MonoBehaviour {
+
+	public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void spawnNext() {
+		int i = Random.Range (0, groups.Length);
+
+		// Spawn Group at current position
+		Instantiate(groups[i], transform.position, Quaternion.identity);
+	}
+}
