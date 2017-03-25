60c393801389ab7fa3605856babf12320d5b8acd
bowsplinter <vivekkalyansk@gmail.com>
2017-03-25 17:20:50 +0800

    Add script to destroy blocks that fall over

---
+++ Assets/Scripts/Destoryer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destoryer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		// collider is square, so we need to check the tag of the parent object
+		if (other.transform.parent.gameObject.tag == "falling") {
+			FindObjectOfType<PlayerController>().SpawnBlock();
+		}
+		// destory parent(group)
+		Destroy(other.transform.parent.gameObject);
+	}
+}
