263f39af106d99837fa96c341a801fc246079a2f
bowsplinter <vivekkalyansk@gmail.com>
2017-03-27 00:20:32 +0800

    Check if object is present before destroying

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroyer : MonoBehaviour {
-	private Collider2D block;
+	private GameObject parentBlock;
 
 	void OnTriggerEnter2D(Collider2D other) {
-		this.block = other;
+		this.parentBlock = other.transform.parent.gameObject;
 		// collider is square, so we need to check the tag of the parent object
 		if (other.transform.parent.gameObject.tag == "falling") {
 			other.GetComponentInParent<BlockController>().SetSpawnNext(true);
@@ -16,7 +16,9 @@ public class Destroyer : MonoBehaviour {
 
 	// Destroy parent block after given number of seconds
 	IEnumerator WaitAndDestroy(float time) {
-		yield return new WaitForSeconds(time);
-		Destroy(this.block.transform.parent.gameObject);
+		while (this.parentBlock != null) {
+			yield return new WaitForSeconds(time);
+			Destroy(this.parentBlock);
+		}
 	}
 }
