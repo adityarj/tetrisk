688963f0c82a73ccc49e6853f67b3289cfbd618c
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-10 15:30:21 +0800

    added meteor powerups and abstract class

---
+++ Assets/Scripts/MeteorPowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorPowerUp : PowerUpController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		GameObject parentBlock = other.transform.parent.gameObject;
+		if (parentBlock.CompareTag("Untagged")) {
+			base.setCollected (true);
+		}
+	}
+}
--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -2,23 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpController : MonoBehaviour {
+public abstract class PowerUpController : MonoBehaviour {
 
 	private bool collected = false;
 
-	private void setCollected(bool collected) {
+	public void setCollected(bool collected) {
 		this.collected = collected;
 	}
 
 	public bool getCollected() {
 		return this.collected;
-	}
-
-	void OnTriggerStay2D(Collider2D other) {
-		GameObject parentBlock = other.transform.parent.gameObject;
-		if (parentBlock.CompareTag("Untagged")) {
-			setCollected(true);
-		}
 	}
 
 	public void DestoryPowerUp() {
