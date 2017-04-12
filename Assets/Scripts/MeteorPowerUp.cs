3064c09a7e8bcd916f34e0cf4c250a04c5107553
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-12 22:07:59 +0800

    fixed transform of red powerup and meteor interference

--- Assets/Scripts/MeteorPowerUp.cs
+++ Assets/Scripts/MeteorPowerUp.cs
@@ -8,6 +8,7 @@ public class MeteorPowerUp : PowerUpController {
 	[SerializeField]
 	private GameObject Meteor;
 	private Rigidbody2D rb;
+	private GameObject actualMeteor;
 
 	// Use this for initialization
 	void Start () {
@@ -16,21 +17,31 @@ public class MeteorPowerUp : PowerUpController {
 
 	public bool checkBounds(Transform playerTransform, Vector3 other) {
 		
-		return playerTransform.position.x + 3 > other.x && playerTransform.position.x - 3 < other.x;
+		return playerTransform.position.x + 4 > other.x && playerTransform.position.x - 4 < other.x;
 	}
 
 	[Command]
 	private void CmdSpawnMeteor(float x, float y) {
-		Meteor = Instantiate (Meteor);
+		
 		NetworkManager networkManager = NetworkManager.singleton;
 		List<Transform> playerPositions = networkManager.startPositions;
 
 		foreach (Transform playerPosition in playerPositions) {
 			if (!this.checkBounds (playerPosition, new Vector3(x,y,0))) {
-				Meteor.transform.position = new Vector3 (playerPosition.transform.position.x - 6, playerPosition.transform.position.y + 16, 0);
-				rb = Meteor.GetComponent<Rigidbody2D> ();
-				rb.velocity = new Vector3 ( (float)6, -4, 0);
-				NetworkServer.Spawn (Meteor);
+				Debug.Log (playerPosition.position);
+				actualMeteor = Instantiate (Meteor);
+
+				if (playerPosition.position.x > 3) {
+					actualMeteor.transform.position = new Vector3 (playerPosition.position.x + 14, playerPosition.position.y + 16, 0);
+					rb = actualMeteor.GetComponent<Rigidbody2D> ();
+					rb.velocity = new Vector3 ((float)-6, -4, 0);
+				} else {
+					actualMeteor.transform.position = new Vector3 (playerPosition.position.x - 14, playerPosition.position.y + 16, 0);
+					rb = actualMeteor.GetComponent<Rigidbody2D> ();
+					rb.velocity = new Vector3 ((float)6, -4, 0);
+				}
+				
+				NetworkServer.Spawn (actualMeteor);
 			}
 		}
 	}
