b034df60eeffbce4fc0126da8b38fc072c198802
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2017-04-15 14:28:28 +0800

    Current ver

---
+++ Assets/Scripts/PowerupRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupRandomizer : PowerUpController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void CmdSpawnRandomizer(){
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		GameObject parentBlock = other.transform.parent.gameObject;
+		if (parentBlock.CompareTag("Untagged")) {
+			base.setCollected (true);
+			this.CmdSpawnRandomizer ();
+			Destroy (gameObject);
+		}
+	}
+}
