3abbe07a46bb67b34efb666674ff7c474f4721b5
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2017-04-16 19:44:11 +0800

    Added title bounce animation

---
+++ Assets/Scripts/BouncingTitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingTitle : MonoBehaviour {
+
+	public bool move = true;
+	public Vector3 MoveVector = Vector3.up;
+	public float MoveRange = 0.1f;
+	public float MoveSpeed = 0.5f;
+
+	private BouncingTitle bouncingTitle;
+
+	Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		bouncingTitle = this;
+		startPosition = bouncingTitle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (move) {
+			bouncingTitle.transform.position = startPosition + MoveVector * (MoveRange * Mathf.Sin (Time.timeSinceLevelLoad * MoveSpeed));
+		}
+	}
+}
