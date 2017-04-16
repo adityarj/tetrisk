e71db759022f4f962b0473d8b32daf8e00a21b69
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-16 21:41:44 +0800

    incorporated new network UI into existing menu

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetworkManagerUI : NetworkManager {
+
+	public void StartupHost() {
+		SetPort ();
+		NetworkManager.singleton.StartHost ();
+	}
+
+	public void JoinClient() {
+		SetPort ();
+		IPAddress ();
+		NetworkManager.singleton.StartClient ();
+	}
+
+	public void SetPort() {
+		NetworkManager.singleton.networkPort = 7777;
+	}
+
+	public void IPAddress() {
+		string IPAddress = "localhost";
+		NetworkManager.singleton.networkAddress = IPAddress;
+	}
+}
