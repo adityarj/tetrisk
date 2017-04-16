5ec80249792d4b1415c45c65cbf854262713a8cd
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2017-04-16 20:02:39 +0800

    Enabled disable audio button

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	//initialized as on
+	bool audioOn = true;
+
+	public void ToggleAudio(){
+		if (audioOn) {
+			AudioListener.pause = true;
+			audioOn = false;
+		} else {
+			AudioListener.pause = false;
+			audioOn = true;
+		}
+
+	}
+}
