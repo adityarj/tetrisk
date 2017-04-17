33f6c31e2f0c826082fe96075d26e9a1e5ecb1c0
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-17 17:47:19 +0800

    added trigger enter destroy script for shield

--- Assets/ShieldController.cs
+++ Assets/ShieldController.cs
@@ -1,7 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ShieldController : MonoBehaviour {
 
+	private bool setCountdown = false;
+
+	void DestroyShield() {
+		NetworkServer.Destroy (gameObject);
+	}
+
+	void OnTriggerEnter2d(Collider2D other) {
+		if (!this.setCountdown) {
+			if (other.gameObject.CompareTag ("Untagged")) {
+				this.setCountdown = true;
+				NetworkServer.Destroy (other.gameObject);
+				Invoke ("DestroyShield", 4);
+			}
+		}
+	}
 }
