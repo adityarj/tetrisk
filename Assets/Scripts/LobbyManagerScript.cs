5101898057d446e75123d476b55384615785ff7f
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2017-04-18 13:57:01 +0800

    Lobby

---
+++ Assets/Scripts/LobbyManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyManagerScript : NetworkLobbyManager {
+
+	static public LobbyManagerScript s_Singleton;
+
+	// Use this for initialization
+	void Start () {
+		s_Singleton = this;
+	}
+}
---
+++ Assets/Scripts/LobbyManagerUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LobbyManagerUI : MonoBehaviour {
+
+	public LobbyManagerScript lobbyManager;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void SetPort() {
+		lobbyManager.networkPort = 7777;
+	}
+
+	public void SetIPAddress() {
+		string IPAddress = GameObject.Find ("HostName").transform.FindChild ("Text").GetComponent<Text> ().text;
+
+		if (IPAddress.Length == 0) {
+			lobbyManager.networkAddress = "localhost";
+		} else {
+			lobbyManager.networkAddress = IPAddress;
+		}
+	}
+
+	public void OnClickServer(){
+		SetPort ();
+		SetIPAddress ();
+		lobbyManager.StartServer();
+	}
+
+	public void OnClickJoin(){
+		SetPort ();
+		SetIPAddress ();
+		lobbyManager.StartClient ();
+	}
+
+	public void OnClickExit(){
+		lobbyManager.StopHost ();
+	}
+
+
+}
