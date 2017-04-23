17a0de9523742bfa0f5b3a3cbd5b74d61b77e748
Vivek Kalyan <vivekkalyansk@gmail.com>
2017-04-23 19:52:32 +0800

    Add simple tests

---
+++ Assets/Editor/BoundsCheckerTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class BoundsCheckerTest {
+
+	[Test]
+	public void checkValidBoundsTotalTest() {
+		double[] bounds = new double[2];
+		bounds[0] = 0;
+		bounds[1] = 10;
+		Assert.IsTrue(BoundsChecker.checkValidBoundsTotal(2,bounds));  
+	}
+
+
+}
